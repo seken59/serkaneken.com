e0747653aca3b55242bbf368147ff164045f7dae
seken59 <51036825+seken59@users.noreply.github.com>
2024-07-11 14:13:23 +0300

    Update category.tsx

--- src/wp-templates/category.tsx
+++ src/wp-templates/category.tsx
@@ -19,7 +19,7 @@ import { FireIcon, FolderIcon } from "@heroicons/react/24/outline";
 const Category: FaustTemplate<PageCategoryGetCategoryQuery> = (props) => {
   // LOADING ----------
   if (props.loading) {
-    return <>Loading...</>;
+    return <>Yükleniyor...</>;
   }
 
   if (!props?.data || !props.data.category) {
@@ -99,7 +99,7 @@ const Category: FaustTemplate<PageCategoryGetCategoryQuery> = (props) => {
                 <div className="flex items-center text-sm font-medium space-x-2 rtl:space-x-reverse cursor-pointer text-neutral-500 dark:text-neutral-400 ">
                   <FireIcon className="w-5 h-5" />
                   <span className="text-neutral-700 dark:text-neutral-300">
-                    {count || 0} Articles
+                    {count || 0} Konu
                   </span>
                 </div>
                 <span className="block text-sm sm:text-base text-neutral-500 dark:text-neutral-400">
