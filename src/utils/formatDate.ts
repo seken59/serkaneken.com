abe1a143b133b96d3807af90270eaeafea04474e
seken59 <51036825+seken59@users.noreply.github.com>
2024-07-11 13:23:36 +0300

    Update formatDate.ts

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -4,7 +4,7 @@ export default function ncFormatDate(date: string) {
 
   let d = new Date(date);
   let ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
-  let mo = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
+  let mo = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(d);
   let da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
   return `${mo} ${da}, ${ye}`;
 }
